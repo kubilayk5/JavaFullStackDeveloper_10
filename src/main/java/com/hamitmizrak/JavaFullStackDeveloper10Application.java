db89eeb77939ca3d8f312f7d7c4859af8ed26891
kubilayk5 <kubilaykizilhan@gmail.com>
2023-08-19 15:48:02 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/JavaFullStackDeveloper10Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFullStackDeveloper10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFullStackDeveloper10Application.class, args);
+	}
+
+}
